f2c4350f8d7d2950a9521a321e391e09d050c7d0
dmitripev <60838539+dmitripev@users.noreply.github.com>
2020-10-05 01:16:18 +0300

    Add files via upload

--- main.c
+++ main.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
-
+#include <math.h>
 int main()
 {
-    float x;
-    scanf("%f", &x);
-    printf("%f", x);
-    return 0;
+    float z, a;
+    scanf("%f", &a);
+    z = 1 - 0.25 * pow((sin(2 * a)), 2) + sin(2 * a);
+    printf("%f\n", z);
+    z = pow((cos(a)), 2) + pow((cos(a)), 4);
+    printf("%f", z);
 }
