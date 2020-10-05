f8d764c68c98f5c6a306f57168c96e6caa50c30d
dmitripev <60838539+dmitripev@users.noreply.github.com>
2020-10-05 16:08:35 +0300

    Update main2.c

--- main2.c
+++ main2.c
@@ -4,7 +4,7 @@ int main()
 {
     float z, a;
     scanf("%f", &a);
-    z = 1 - 0.25 * pow((sin(2 * a)), 2) + sin(2 * a);
+    z = 1 - (1 / 4.00) * pow((sin(2 * a)), 2) + sin(2 * a);
     printf("%f\n", z);
     z = pow((cos(a)), 2) + pow((cos(a)), 4);
     printf("%f", z);
