402f7470dc4799c2e63b3d646f3adc92dd189149
dmitripev <60838539+dmitripev@users.noreply.github.com>
2020-10-05 16:50:24 +0300

    Update main4_2.c

--- main4_2.c
+++ main4_2.c
@@ -3,14 +3,11 @@ int main()
 {
     int x;
     scanf("%d", &x);
-    int arr[32];
-    int i = 0;
-    while (x > 0)
-    {
-         arr[i] = x % 2;
-         x = x / 2;
-         ++i;
-    }
-    printf("%d", arr[i - 5]);
+    int x;
+    x = x >> 5;
+    x = x & 1;
+    printf("%d", x);
+    
+
 	return 0;
 }
